5221bd8be42a66fc6e2b34f3cc9b1b7c60834eba
hossein babazadeh <hosseinb@hosseins-MacBook-Air.local>
2022-11-17 10:54:26 +0330

    first loop added

--- heart.c
+++ heart.c
@@ -3,5 +3,13 @@
 int main(){
     int n;
     scanf("%d", &n);
-    int star = n/2, space;    
+    int star = n/2, space;
+    while(star <= n){
+        space = (n-star)/2;
+        int cnt = space;
+        while(cnt > 0){
+            printf(" ");
+            cnt--;
+        }
+    }    
 }
